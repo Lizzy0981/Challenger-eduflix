26b82d92da5946f21bfd2cc5794b63ee57e4b828
Elizabeth Diaz Familia <lizzyfamilia@gmail.com>
2025-01-16 17:52:25 -0600

    Update index.js

--- src/components/common/NotificationSystem/index.js
+++ src/components/common/NotificationSystem/index.js
@@ -1,23 +1,13 @@
-import Banner from './Banner/Banner'
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import Boton from './Button/Boton'
-import Carrusel from './Carrusel/Carrusel'
-import FormVideo from './Formularios/FormVideo'
-import FormCategoria from './Formularios/FormCategoria'
-import DrawerComponent from './Header/DrawerComponent'
-import VideoCard from './Carrusel/VideoCard'
-import FormSearch from './Formularios/FormSearch'
+import NotificationSystem, { 
+  NotificationProvider, 
+  useNotifications, 
+  NOTIFICATION_TYPES_EXPORT 
+} from './NotificationSystem'
 
-export {
-  Banner,
-  Header,
-  Footer,
-  Boton,
-  Carrusel,
-  FormCategoria,
-  FormVideo,
-  DrawerComponent,
-  VideoCard,
-  FormSearch
+export default NotificationSystem
+export { 
+  NotificationProvider, 
+  useNotifications, 
+  NOTIFICATION_TYPES_EXPORT 
 }
+
