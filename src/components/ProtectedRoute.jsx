26cf1c0087a9132531423acaebf287dc6f52dd14
Elizabeth Diaz Familia <lizzyfamilia@gmail.com>
2025-01-24 19:20:44 -0600

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate, useLocation } from 'react-router-dom'
-import { useAuth } from '@context/useAuth'
+import { useAuth } from '@Context/useAuth'
 import LoadingComponent from './LoadingComponent'
 
 function ProtectedRoute({ children, requiredRole = null }) {
