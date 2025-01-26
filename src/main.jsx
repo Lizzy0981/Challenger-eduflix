bd8fc316037999ef83d80ac78c68369c03abd6a1
Elizabeth Diaz Familia <lizzyfamilia@gmail.com>
2025-01-26 13:27:18 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import { VideosProvider } from '@Context/Context'
-import { NotificationProvider } from '@components/common/NotificationSystem/NotificationSystem'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
+import CircularProgress from '@mui/material/CircularProgress'
 import theme from './theme'
 import App from './App'
 import './index.css'
+
+const VideosProvider = React.lazy(() => import('@Context/Context').then(module => ({ default: module.VideosProvider })))
+const NotificationProvider = React.lazy(() => import('@components/common/NotificationSystem/NotificationSystem').then(module => ({ default: module.NotificationProvider })))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <VideosProvider>
-        <NotificationProvider>
-          <App />
-        </NotificationProvider>
-      </VideosProvider>
+      <Suspense fallback={<CircularProgress />}>
+        <VideosProvider>
+          <NotificationProvider>
+            <App />
+          </NotificationProvider>
+        </VideosProvider>
+      </Suspense>
     </ThemeProvider>
   </React.StrictMode>
 )
